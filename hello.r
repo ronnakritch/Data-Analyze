c6b9c6064121492e34f67956dbdba472ac5033dc
ronnakritch <ronnakrit.chidcharoen@gmail.com>
2024-05-27 22:20:29 +0700

    Update hello.r

--- hello.r
+++ hello.r
@@ -1,2 +1,22 @@
 #this is r programing
 print('hello world')
+
+print("this is a second line")
+
+library(dplyr)
+
+df<- read_csv("filename.csv")
+
+df%>%
+  filter(col>100)
+
+#update dplyr function
+df2 <- df %>%
+  select(1:10)%>%
+  filter(country=="Thailand")%>%
+  arrange(-income)
+
+#plot new histogram
+library(ggplot2)
+ggplot(df2, aes(income)) + 
+geom_histrogram()
